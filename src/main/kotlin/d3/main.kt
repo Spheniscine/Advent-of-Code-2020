25c7a6607d67ed4ef718dbf14807e811a7b33726
Spheniscine <spheniscine@gmail.com>
2020-12-03 13:14:53 +0800

    day 3

--- src/main/kotlin/d3/main.kt
+++ src/main/kotlin/d3/main.kt
@@ -6,17 +6,34 @@ import java.io.File
 private val input by lazyInput(3, "gmail")
 
 fun main() {
-    println("--- Day 3 ---")
+    println("--- Day 3: Toboggan Trajectory ---")
     markTime()
 
-    val ans1 = "test"
+    val G = input.lines()
+    val width = G[0].length
+    val height = G.size
+
+    fun f(right: Int, down: Int): Int {
+        var ans = 0
+
+        var j = 0
+        for(i in down until height step down) {
+            j = (j + right) % width
+            if(G[i][j] == '#') ans++
+        }
+
+        return ans
+    }
+
+    val ans1 = f(3, 1)
 
     println("Part 1: $ans1")
     printTime()
 
-//    markTime()
-//
-//
-//    println("Part 2: $ans2")
-//    printTime()
+    markTime()
+
+    val ans2 = f(1, 1).toLong() * ans1 * f(5, 1) * f(7, 1) * f(1, 2)
+
+    println("Part 2: $ans2")
+    printTime()
 }
