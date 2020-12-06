e9f75395ed3c174c1080ae7d80b61685fa86350a
Spheniscine <spheniscine@gmail.com>
2020-12-06 17:20:01 +0800

    Day 6: Custom Customs

--- src/main/kotlin/d6/main.kt
+++ src/main/kotlin/d6/main.kt
@@ -6,17 +6,39 @@ import java.io.File
 private val input by lazyInput(6, "gmail")
 
 fun main() {
-    println("--- Day 6 ---")
+    println("--- Day 6: Custom Customs ---")
     markTime()
 
-    val ans1 = "test"
+    val groupStrings = input.split("\n\n")
+
+    var ans1 = 0
+
+    for(s in groupStrings) {
+        val present = BooleanArray(26)
+        for(c in s) {
+            if(c in 'a'..'z') present[c - 'a'] = true
+        }
+        ans1 += present.count { it }
+    }
 
     println("Part 1: $ans1")
     printTime()
 
-//    markTime()
-//
-//
-//    println("Part 2: $ans2")
-//    printTime()
+    markTime()
+
+    val groups = groupStrings.map { it.lines() }
+
+    var ans2 = 0
+
+    for(g in groups) {
+        val cnt = IntArray(26)
+        for(s in g) for(c in s) {
+            cnt[c - 'a']++
+        }
+        val m = g.size
+        ans2 += cnt.count { it == m }
+    }
+
+    println("Part 2: $ans2")
+    printTime()
 }
---
+++ src/main/kotlin/d8/main.kt
@@ -0,0 +1,22 @@
+package d8
+
+import commons.*
+import java.io.File
+
+private val input by lazyInput(8, "gmail")
+
+fun main() {
+    println("--- Day 8 ---")
+    markTime()
+
+    val ans1 = "test"
+
+    println("Part 1: $ans1")
+    printTime()
+
+//    markTime()
+//
+//
+//    println("Part 2: $ans2")
+//    printTime()
+}
