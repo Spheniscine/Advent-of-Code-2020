9d74ff1ba1e94b5c2578af414f2fb5cc05e4545c
Spheniscine <spheniscine@gmail.com>
2020-12-15 14:51:32 +0800

    day 15

--- src/main/kotlin/d15/main.kt
+++ src/main/kotlin/d15/main.kt
@@ -1,7 +1,6 @@
 package d15
 
 import commons.*
-import java.io.File
 
 private val input by lazyInput(15, "gmail")
 
@@ -9,14 +8,48 @@ fun main() {
     println("--- Day 15 ---")
     markTime()
 
-    val ans1 = "test"
+    val A = input.split(',').map { it.toInt() }
+    val map = HashMap<Int, ArrayDeque<Int>>().autoPut { ArrayDeque(2) }
+
+    for(i in A.indices) {
+        val a = A[i]
+        val d = map[a]
+        if(d.size > 1) d.removeFirst()
+        d.add(i)
+    }
+
+    val ans1 = run {
+        var a = A.last()
+        for(i in A.size until 2020) {
+            var d = map[a]
+            a = if(d.size > 1) i-1-d.first() else 0
+            d = map[a]
+            if(d.size > 1) d.removeFirst()
+            d.add(i)
+        }
+
+        a
+    }
 
     println("Part 1: $ans1")
     printTime()
 
-//    markTime()
-//
-//
-//    println("Part 2: $ans2")
-//    printTime()
+    markTime()
+
+    val ans2 = run {
+        var a = ans1
+        for(i in 2020 until 30000000) {
+            var d = map[a]
+            a = if(d.size > 1) i-1-d.first() else 0
+            d = map[a]
+            if(d.size > 1) d.removeFirst()
+            d.add(i)
+        }
+
+        a
+    }
+
+
+    println("Part 2: $ans2")
+    printTime()
 }
